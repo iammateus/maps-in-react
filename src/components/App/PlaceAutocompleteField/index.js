334b301ee9e830247fca03afa31e4c639d8f9612
Mateus Soares <mateussoaress1997@gmail.com>
2020-03-07 14:03:52 -0300

    Project restructuring and PlaceAutocompleteField component created

---
+++ src/components/App/PlaceAutocompleteField/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PlaceAutocompleteField() {
+    return (
+        <div>
+            <label>
+                <input placeholder="Inform location here" type="text" />
+            </label>
+            <ul>
+                
+            </ul>
+        </div>
+  );
+}
+
+export default PlaceAutocompleteField;
---
+++ src/components/App/index.js.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PlaceAutocompleteField from './PlaceAutocompleteField';
+
+function App() {
+  return (
+    <div className="App">
+      <PlaceAutocompleteField />
+    </div>
+  );
+}
+
+export default App;
