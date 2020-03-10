03765569f8ac76a8a55054ab7fee3a8e8d9d98a7
Mateus Soares <mateus.silva@pravaler.com.br>
2020-03-10 18:04:05 -0300

    Improving map appearance

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import PlaceAutocompleteField from "../PlaceAutocompleteField";
+import { IconContext } from "react-icons";
+import { MdPlace } from 'react-icons/md';
 
 import StyledMap from "./styled/StyledMap";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = () => (<IconContext.Provider value={{ color: "red", className: "global-class-name", size: '3em' }}>
+                                    <div>
+                                        <MdPlace />
+                                    </div>
+                                </IconContext.Provider>)
 
 function Map() {
 
@@ -13,7 +19,7 @@ function Map() {
             lat: -23.51395429999999,
             lng: -46.38868249999999
         },
-        zoom: 11
+        zoom: 18
     });
 
     return (
@@ -30,7 +36,6 @@ function Map() {
                     <AnyReactComponent
                         lat={mapOptions.center.lat}
                         lng={mapOptions.center.lng}
-                        text="My Marker"
                     />
                 </GoogleMapReact>
             </div>
